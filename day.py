c35f4ea0581f4a0dda891ad415f9143dda1497e9
onesuper <onesuperclark@gmail.com>
2016-07-05 19:28:20 +0800

    add -f

--- day.py
+++ day.py
@@ -8,9 +8,13 @@ import sys
 MD_FILE=str(date.today()) + '.md'
 
 if os.path.isfile(MD_FILE):
-  print MD_FILE + ' already exists' 
-  sys.exit(2)  
+  if len(sys.argv) > 1 and sys.argv[1] == '-f':
+    print 'overwrite!'
+  else:
+  	print MD_FILE + ' already exists. (Use -f)'   
+  	sys.exit(2)  
 
 with open('template.md') as t:
   with open(MD_FILE, 'w') as f:
     f.writelines(t.readlines())
+    print  os.getcwd() + '/' + MD_FILE
