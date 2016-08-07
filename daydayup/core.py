ed4508178fccc11749aee69597ea31148110a366
onesuper <onesuperclark@gmail.com>
2016-08-07 18:35:42 +0800

    create file/directory class

--- daydayup/core.py
+++ daydayup/core.py
@@ -1,10 +1,12 @@
 # -*- coding: UTF-8 -*-
 
-from argparse import ArgumentParser
-from pkg_resources import resource_filename, load_entry_point
-
 from daydayup import __version__
 from daydayup.exception import DayDayUpException
+from daydayup.filesystem import File, Directory
+
+import os
+import sys
+from argparse import ArgumentParser
 
 from datetime import date
 TODAY_REPORT_FNAME = str(date.today()) + '.md'
@@ -12,33 +14,6 @@ TODAY_REPORT_FNAME = str(date.today()) + '.md'
 import yaml
 with open('config.yaml') as f:
   CONF = yaml.safe_load(f)
-
-import os
-import sys
-
-def send_mail(server, fro, to, subject, text): 
-  from email.mime.multipart import MIMEMultipart
-  from email.MIMEText import MIMEText
-  from email.utils import COMMASPACE,formatdate
-  from email import encoders
-
-  assert type(server) == dict 
-  assert type(to) == list 
-
-  msg = MIMEMultipart() 
-  msg['From'] = fro 
-  msg['Subject'] = subject 
-  msg['To'] = COMMASPACE.join(to)
-  msg['Date'] = formatdate(localtime=True) 
-  msg.attach(MIMEText(text, 'html', 'utf-8'))
-
-  import smtplib 
-  smtp = smtplib.SMTP(server['name']) 
-  smtp.starttls()
-  smtp.login(server['email'], server['passwd']) 
-  smtp.sendmail(fro, to, msg.as_string()) 
-  smtp.close()
-
 
 def yes_or_no(question, default="no"):
   valid = {"yes": True, "y": True, "ye": True,
@@ -60,8 +35,35 @@ def yes_or_no(question, default="no"):
     elif choice in valid:
       return valid[choice]
     else:
-      sys.stdout.write("请回答 'yes' 或 'no' "
-                       "(或 'y' 或 'n').\n")
+      sys.stdout.write("请回答 'yes' 或 'no' (或 'y' 或 'n').\n")
+
+
+def send_mail(server, fro, to, subject, text): 
+  from email.mime.multipart import MIMEMultipart
+  from email.MIMEText import MIMEText
+  from email.utils import COMMASPACE,formatdate
+  from email import encoders
+
+  assert type(server) == dict 
+  assert type(to) == list 
+
+  tos = COMMASPACE.join(to)
+
+  prompt = '一封主题为"' + subject.encode('utf-8') + '"的邮件将发送给 '+ tos
+  if yes_or_no(prompt + '\n你真的确定要发送吗?'):
+    msg = MIMEMultipart() 
+    msg['From'] = fro 
+    msg['Subject'] = subject 
+    msg['To'] = tos
+    msg['Date'] = formatdate(localtime=True) 
+    msg.attach(MIMEText(text, 'html', 'utf-8'))
+
+    import smtplib 
+    smtp = smtplib.SMTP(server['name']) 
+    smtp.starttls()
+    smtp.login(server['email'], server['passwd']) 
+    smtp.sendmail(fro, to, msg.as_string()) 
+    smtp.close()
 
 
 def get_github_commits(github_id, github_password):
@@ -101,15 +103,16 @@ class DayDayUp(object):
   """今日生成日报"""
   def _new(self):
 
-    if os.path.isfile(TODAY_REPORT_FNAME):
-      if self.opts['overwrite']:
-        sys.stderr.write('覆盖老的日报!\n')
+    outf = File(self.opts['output'])
+    if outf.exists:
+      if self.opts['force']:
+        sys.stderr.write('强制覆盖老的日报!\n')
+        outf.rm()
       else:
-        raise DayDayUpException(TODAY_REPORT_FNAME + ' 已经存在. (覆盖请使用 --overwrite)\n')
-
-    commits = []
+        raise DayDayUpException(str(outf) + ' 已经存在. (强制覆盖请使用 --force)\n')
 
     # github commits
+    commits = []
     if CONF['github']['enable']:
       commits = get_github_commits(CONF['github']['username'], CONF['github']['password'])
 
@@ -118,32 +121,31 @@ class DayDayUp(object):
     report = {}
     report['files'] = (glob.glob1('.','*-*-*.md'))
 
-    with open(TODAY_REPORT_FNAME, 'w') as f:
-      s = render_template('template.md', {'report': report, 'commits': commits})
-      f.write(s)
-      sys.stderr.write(os.getcwd() + '/' + TODAY_REPORT_FNAME + '\n') 
+    # render and make file
+    content = render_template(self.opts['template'], {'report': report, 'commits': commits})
+    outf.content = content
+    outf.mk()
+    sys.stderr.write('请认真编辑今天的日报: '+ str(outf) + '\n') 
           
   """发送今日日报"""  
   def _send(self):
-    with open(TODAY_REPORT_FNAME) as f:
-      s = '\n'.join(f.readlines()).decode('utf-8')
+    inf = File(self.opts['file'])
 
-      import markdown
-      html = markdown.markdown(s)
+    import markdown
+    html = markdown.markdown(inf.content)
 
-      if self.opts['preview']:
-        sys.stdout.write(html)
-        sys.stdout.write('\n\n')
-      else:
-        sys.stdout.write('友情提示：使用 --preview 检查一遍错别字哦 ^_^\n')
+    if self.opts['preview']:
+      sys.stdout.write(html)
+      sys.stdout.write('\n\n')
+    else:
+      sys.stdout.write('友情提示：使用 --preview 检查一遍错别字哦 ^_^\n')
 
-      # 主题 
-      subject = '-'.join(['DailyReport', str(date.today()), conf['nickname']])
-      mail_from = CONF['server']['email']
-      mail_to   = CONF['dl']
+    # 主题 
+    subject = '-'.join(['DailyReport', str(date.today()), CONF['nickname']])
+    mail_from = CONF['server']['email']
+    mail_to   = CONF['dl']
 
-      if yes_or_no('日报将发送给 '+ ','.join(mail_to) + '\n你真的确定要发送吗?'):
-        send_mail(CONF['server'], mail_from, mail_to, subject, html)
+    send_mail(CONF['server'], mail_from, mail_to, subject, html)
 
   def _get_opts(self, args):
     opts = {}
@@ -152,14 +154,18 @@ class DayDayUp(object):
     parser.add_argument('-v', '--version', action = 'version', version = '{0}'.format(__version__),
                         help = 'show %(prog)s\'s version')
     
-
     new = subparser.add_parser('new')
     new.set_defaults(func = self._new)
-    new.add_argument('--overwrite', action = 'store_true', help = '覆盖已经存在的日报')
-    
+    new.add_argument('--force', '-f', action = 'store_true', help = '覆盖已经存在的日报')
+    new.add_argument('--template', '-t', default = 'daily.md', help = '指定日报的模板')
+    new.add_argument('--output', '-o', default = TODAY_REPORT_FNAME, help = '要生成的日报文件')
+
+
     send = subparser.add_parser('send')
     send.set_defaults(func = self._send)
     send.add_argument('--preview', action = 'store_true', help = '预览发送的内容')
+    send.add_argument('--file', '-f', default = TODAY_REPORT_FNAME, help = '指定要发送的文件')
+
 
     # process the opts
     for option, value in vars(parser.parse_args(args)).iteritems():
